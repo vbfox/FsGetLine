759d41f5938a4653280462bcabe2bc15d19fcc5d
Julien Roncaglia <julien@roncaglia.fr>
2015-03-09 00:38:59 +0100

    AutoComplete sample

--- FsGetLine/Program.fs
+++ FsGetLine/Program.fs
@@ -5,7 +5,19 @@
 
         [<EntryPoint>]
         let main argv = 
-            let mutable editor = FsGetLine.create (fun s -> { s with AppName = Some("foo") })
+            let autoComplete (str:String) cursor =
+                if str.StartsWith("test") && cursor = 4 then
+                    { FsGetLine.Completion.Result = ["42"]; FsGetLine.Completion.Prefix = "" }
+                else if str.StartsWith("x") && cursor = 1 then
+                    { FsGetLine.Completion.Result = ["y";"x";"MarkTheSpot"]; FsGetLine.Completion.Prefix = "x" }
+                else
+                    { Result = []; Prefix = "" }
+            let mutable editor = FsGetLine.create (fun s ->
+                {
+                    s with
+                        AppName = Some("FsGetLineSample")
+                        AutoCompleteEvent = Some(autoComplete)
+                })
             let mutable s = Some("")
             while s.IsSome do
                 let (newEditor, line) = editor |> FsGetLine.get "F#" ""
